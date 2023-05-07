cf2a3e1b8014df1148dadaf4372f553748cf5eb3
Robin Mordasiewicz <robin@mordasiewicz.com>
2023-05-07 17:13:05 -0400

    small changes

--- doit.sh
+++ doit.sh
@@ -4,5 +4,6 @@
 jq -r 'keys[]' themes.json |  while read i; do
   npm install jsonresume-theme-${i}
   npx resume export Robin-Mordasiewicz-${i}.pdf --theme ./node_modules/jsonresume-theme-${i} --format pdf resume.json
+  npx resume export Robin-Mordasiewicz-${i}.html --theme ./node_modules/jsonresume-theme-${i} --format html resume.json
 done
 
